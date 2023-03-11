b8616aeaec2ffc136c43f43bf7982b8f2bebe761
marvmelo <marcusvmelodev@gmail.com>
2023-03-10 22:29:15 -0300

    done

--- monitoring/app/api/endpoints/aderencia.py
+++ monitoring/app/api/endpoints/aderencia.py
@@ -1,8 +1,24 @@
 """Endpoint para cálculo de aderência."""
 from fastapi import APIRouter
+from scipy.stats import ks_2samp
+import os
+from .misc import *
 
 router = APIRouter(prefix="/aderencia")
 
 @router.post("/")
-def aderencia():
-    return "Hello aderencia"
+def aderencia(register_path: RegistroPath):
+
+    df = create_dataframe_from_path(dir_path+"/files/"+register_path.path)
+    df_test = pd.read_csv(dir_path+"/files/test", low_memory=False)
+    model = load_model()
+    
+    X = clean_df(model, df)
+    _, y_test = separate_x_y(df_test)
+    predicted_y = model.predict_proba(X)[:, 1]
+    ks = ks_2samp(y_test, predicted_y)
+
+    aderencia_response = AderenciaResponse(ks=ks.statistic,
+                                           test=y_test.to_list(),
+                                           other=predicted_y.tolist())
+    return aderencia_response
--- monitoring/app/api/endpoints/misc.py
+++ monitoring/app/api/endpoints/misc.py
@@ -2,9 +2,18 @@ import pickle
 import sklearn
 from pydantic import BaseModel
 import pandas as pd
+import os
+
+dir_path = os.path.dirname(__file__)
+
+def clean_df(model, df: pd.DataFrame):
+    for column in df.columns:
+        if column not in model.feature_names_in_:
+            df.drop(axis=1, labels=[column], inplace=True, errors='ignore')
+    return df
 
 def load_model():
-    with open("model.pkl", "rb") as model_file:
+    with open(dir_path+"/files/model.pkl", "rb") as model_file:
         model = pickle.load(model_file)
     return model
 
@@ -14,19 +23,33 @@ def create_dataframe(registers: list, remove_NaN=False):
     if remove_NaN: df.dropna(axis=0, inplace=True)
     return df
 
+def create_dataframe_from_path(path: str, remove_NaN=False):
+    df = pd.read_csv(path, low_memory=False)
+    df["REF_DATE"] = pd.to_datetime(df["REF_DATE"])
+    if remove_NaN: df.dropna(axis=0, inplace=True)
+    return df
+
 def separate_x_y(df: pd.DataFrame):
-    X = df.drop(axis=1, labels=["REF_DATE", "TARGET"])
+    X = df.drop(axis=1, labels=["REF_DATE", "TARGET"], errors='ignore')
     y = df["TARGET"]
     return X, y
 
 class Registro(BaseModel):
     registers: list
 
+class RegistroPath(BaseModel):
+    path: str
+
 class PerformanceResponse(BaseModel):
     month_frequency: dict
     auc: float
     FPRs: list
     TPRs: list
+
+class AderenciaResponse(BaseModel):
+    ks: float
+    test: list
+    other: list
 
 if __name__=="__main__":
     model = load_model()
